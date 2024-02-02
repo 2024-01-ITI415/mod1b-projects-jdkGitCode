ed9a8d085d945480bed4c2e4c8fed09badd9356e
jdkGitCode <125212768+jdkGitCode@users.noreply.github.com>
2024-02-01 21:39:30 -0500

    Added Basket movement and Score texts

---
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 pos = transform.position;
+        pos.x = mousePos3D.x;
+        transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.CompareTag("Apple"))
+        {
+            Destroy(other);
+        }
+    }
+}
