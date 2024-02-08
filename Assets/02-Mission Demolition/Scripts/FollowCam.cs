a91d7f6278862e3fecb92bfcc969618c8af2041e
jdkGitCode <125212768+jdkGitCode@users.noreply.github.com>
2024-02-08 13:45:37 -0500

    Touched up camera movement to make it smoother

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Set in inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set dynamically")]
     public float camZ;
 
@@ -22,9 +26,14 @@ public class FollowCam : MonoBehaviour
         }
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
 
         destination.z = camZ;
 
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
